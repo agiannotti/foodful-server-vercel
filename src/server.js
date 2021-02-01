7040d7e83c993442c42ba1af648360ff32ccf749
tonyg <aj.gianno@gmail.com>
2021-02-01 15:23:09 -0600

    env

--- src/server.js
+++ src/server.js
@@ -2,9 +2,10 @@
 const app = require('./app');
 const knex = require('knex');
 const { PORT, DATABASE_URL } = require('./config');
+const pg = require('pg');
 
 // console.log('CONNECTION', DATABASE_URL);
-
+pg.defaults.ssl = process.env.NODE_ENV === 'production';
 const db = knex({
   client: 'pg',
   connection: DATABASE_URL,
